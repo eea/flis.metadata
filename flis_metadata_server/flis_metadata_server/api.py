f4ba1179c681fd5b573b856fbc40c9a212afd824
Mihai Bivol <mm.bivol@gmail.com>
2014-11-03 12:53:17 +0200

    Add django app with basic API

---
+++ flis_metadata_server/flis_metadata_server/api.py
@@ -0,0 +1,39 @@
+"""Dynamically add resources for ReplicatedModel classes in models.py."""
+import inspect
+import sys
+
+from flis_metadata_server.models import ReplicatedModel
+
+from tastypie.resources import ModelResource
+
+
+def is_replicated_model(cls):
+    """Check wether a class is a model to be replicated.
+
+    This is implementend by extracting all models that inherit from
+    ReplicatedModel
+    """
+    return (inspect.isclass(cls) and
+            issubclass(cls, ReplicatedModel) and
+            cls != ReplicatedModel)
+
+
+replicated_models = inspect.getmembers(
+    sys.modules['flis_metadata_server.models'], is_replicated_model)
+
+
+def build_resource(model_name, cls):
+    """Build a resource from a base class."""
+    name = model_name + "Resource"
+
+    # Build meta class
+    Meta = type('Meta', (object, ), {
+        'allowed_methods': ['get'],
+        'queryset': cls.objects.all(), })
+
+    Resource = type(name, (ModelResource, ), {'Meta': Meta})
+
+    return Resource
+
+# Append any other custom resources to this list
+resources = [build_resource(name, cls) for name, cls in replicated_models]
---
+++ flis_metadata_server/flis_metadata_server/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from tastypie.api import Api
+
+from flis_metadata_server.api import resources
+
+v1_api = Api(api_name="v1")
+for resource in resources:
+    v1_api.register(resource())
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(v1_api.urls)),
+)
