da357220cb3973c08fff7a19bd5b84d13f4ec0e4
Mihai Bivol <mm.bivol@gmail.com>
2014-11-10 15:50:16 +0200

    Add support for frame auth

--- flis_metadata/server/settings.py
+++ flis_metadata/server/settings.py
@@ -47,11 +47,11 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'frame.middleware.RequestMiddleware',
     'frame.middleware.UserMiddleware',
-    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
