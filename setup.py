2a20905c2d58635a690631f8872dd124b6cb8142
Mihai Bivol <mm.bivol@gmail.com>
2014-11-10 16:26:03 +0200

    Add pip setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-flis-metadata-client',
+    name='eea.flis.metadata',
     version='0.1',
     packages=['flis_metadata',
               'flis_metadata.client',
@@ -20,6 +20,7 @@ setup(
     description='EEA Metadata models replication support',
     long_description=README,
     author='Mihai Bivol',
+    url='https://github.com/eea/flis.metadata',
     author_email='mm.bivol@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
