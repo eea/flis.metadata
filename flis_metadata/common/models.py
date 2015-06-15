f31a6b31f0417f553d07d60b367d2de875bdc637
Ana Ion <ana.ion4@gmail.com>
2015-06-15 16:28:37 +0300

    [sortable-lists] Add the sort_id parameter. Update fixtures. Add migration. Refs #25881

--- flis_metadata/common/models.py
+++ flis_metadata/common/models.py
@@ -1,10 +1,9 @@
 import inspect
 import sys
 
-from django.db.models import BooleanField
-from django.db.models import CharField
-from django.db.models import Model
-
+from django.db.models import (
+    BooleanField, CharField, Model, IntegerField,
+)
 
 class ReplicatedModel(Model):
 
@@ -21,9 +20,10 @@ class GeographicalScope(ReplicatedModel):
 
     title = CharField(max_length=128)
     require_country = BooleanField(default=False)
+    sort_id = IntegerField(default=0, null=True, blank=True)
 
     class Meta:
-        ordering = ('-pk',)
+        ordering = ('sort_id',)
 
     def __unicode__(self):
         return self.title
@@ -32,9 +32,10 @@ class GeographicalScope(ReplicatedModel):
 class EnvironmentalTheme(ReplicatedModel):
 
     title = CharField(max_length=128)
+    sort_id = IntegerField(default=0, null=True, blank=True)
 
     class Meta:
-        ordering = ('-pk',)
+        ordering = ('sort_id',)
 
     def __unicode__(self):
         return self.title
@@ -44,6 +45,10 @@ class Country(ReplicatedModel):
 
     iso = CharField(max_length=2, primary_key=True)
     name = CharField(max_length=128)
+    sort_id = IntegerField(default=0, null=True, blank=True)
+
+    class Meta:
+        ordering = ('sort_id',)
 
     def __unicode__(self):
         return self.name
