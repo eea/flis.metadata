6307828f84fe0b2e0ba4c704de5149f5ba271217
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2016-10-13 17:49:27 +0300

    [misc] Update gitignore, bump version to reflect readme changed.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='eea.flis.metadata',
-    version='0.6.1',
+    version='0.6.2',
     packages=['flis_metadata',
               'flis_metadata.client',
               'flis_metadata.client.management',
