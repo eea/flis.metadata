969a716124636c988c47f52574827da4184ee351
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2016-11-02 16:02:47 +0200

    --fake-initial for existing database

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./manage.py migrate
+./manage.py migrate --fake-initial
 ./manage.py load_metadata_fixtures
 ./manage.py collectstatic --noinput
 gunicorn flis_metadata.server.wsgi:application --bind 0.0.0.0:8000
